eb8d2046d2d6d7a90f2d379cb6498f66b48a6a26
RodoAL <rodo.aparicio.lopez@gmail.com>
2024-12-13 03:50:46 -0600

    Agregar configuración de Angular, componentes de diseño y rutas iniciales para el dashboard

--- BolsiVibe/src/app/shared/components/layout/layout.component.ts
+++ BolsiVibe/src/app/shared/components/layout/layout.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { FoooterComponent } from '../foooter/foooter.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [HeaderComponent, SidebarComponent, FoooterComponent, RouterOutlet],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
-export class LayoutComponent {
+export default class LayoutComponent {
 
 }
